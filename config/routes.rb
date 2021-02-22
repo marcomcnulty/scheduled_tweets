2c2636a8f3be2c345ee6ad50c9a2a3e0a0cb0db5
Marco McNulty <marcomcnultycodes@gmail.com>
2021-02-22 21:25:47 +0000

    📦 NEW: add sign in, sign out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   get "about", to: "about#index"
 
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  delete "logout", to: "sessions#destroy"
 end
