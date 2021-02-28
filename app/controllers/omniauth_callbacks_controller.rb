f39b08f1c371265d93cfd4055878f8fbb076c25c
Marco McNulty <marcomcnultycodes@gmail.com>
2021-02-28 15:14:22 +0000

    📦 NEW: twitter account model

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,24 @@
 class OmniauthCallbacksController < ApplicationController
   def twitter
-    render plain: "success"
+    @twitter_account = Current.user.twitter_accounts.where(username: auth.nickname).first_or_initialize
+    set_twitter_account
+    redirect_to root_path, notice: "Successfully connected your account"
+  end
+
+  private
+
+  def set_twitter_account
+    @twitter_account.update(
+      name:     auth.name,
+      username: auth.nickname,
+      image:    auth.image,
+      token:    auth.token,
+      secret:   auth.secret
+    )
+  end
+
+  # Omniauth's way of accessing the credentials
+  def auth
+    request.env["omniauth.auth"].info
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@
 # password:string virtual
 # password_confirmation:string virtual
 class User < ApplicationRecord
+  has_many :twitter_accounts
   has_secure_password
 
-  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address" }
+  validates :email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address"}
 end
