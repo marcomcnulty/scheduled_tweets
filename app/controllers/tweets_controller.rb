4378ff882abef0e306e9815188c385a8e66df704
Marco McNulty <marcomcnultycodes@gmail.com>
2021-03-13 10:44:02 +0000

    📦 NEW: form partial, destroy action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_tweet, except: [:index, :new, :create]
 
   def index
     @tweets = Current.user.tweets
@@ -7,6 +8,9 @@ class TweetsController < ApplicationController
 
   def new
     @tweet = Tweet.new
+  end
+
+  def edit
   end
 
   def create
@@ -18,9 +22,26 @@ class TweetsController < ApplicationController
     end
   end
 
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "Tweet was scheduled successfully"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweet is no longer scheduled"
+  end
+
   private
 
   def tweet_params
     params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
+
+  def set_tweet
+    @tweet = Current.user.tweets.find(params[:id])
   end
 end
